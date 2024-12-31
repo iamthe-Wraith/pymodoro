d264b646875e9a9846424f1b113d37621395c62a
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-12-30 19:58:16 -0500

    feat: adds support for resetting timer

--- main.py
+++ main.py
@@ -2,34 +2,11 @@ import tkinter as tk
 from config import *
 
 reps = 0
+timer = None
 
 window = tk.Tk()
 window.title("Pymodoro")
 window.config(padx=100, pady=50, bg=BACKGROUND_COLOR)
-
-def start_timer():
-    '''
-    Starts the pomodoro timer. Increments the reps counter and calls the countdown function
-    passing the appropriate time based on the reps counter.
-
-    Args:
-        None
-
-    Returns:
-        None
-    '''
-    global reps
-    reps += 1
-
-    if reps % 8 == 0:
-        header.config(text="Break", fg="red")
-        countdown(LONG_BREAK_SECONDS)
-    elif reps % 2 == 0:
-        header.config(text="Break", fg="yellow")
-        countdown(SHORT_BREAK_SECONDS)
-    else:
-        header.config(text="Work", fg="green")
-        countdown(WORK_SECONDS)
 
 def countdown(time_remaining):
     '''
@@ -51,9 +28,55 @@ def countdown(time_remaining):
     time = f"{minutes}:{seconds}"
     canvas.itemconfig(timer_text, text=time)
     if time_remaining > 0:
-        window.after(1000, countdown, time_remaining - 1)
+        global timer
+        timer = window.after(1000, countdown, time_remaining - 1)
     else:
         start_timer()
+        checkmarks.config(text=f"✓" * (reps // 2))
+
+def reset_timer():
+    '''
+    Resets the application to its initial state.
+
+    Args:
+        None
+
+    Returns:
+        None
+    '''
+    
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    header.config(text="Pymodoro!", fg="white")
+    checkmarks.config(text="")  
+    global reps
+    reps = 0
+
+def start_timer():
+    '''
+    Starts the pomodoro timer. Increments the reps counter and calls the countdown function
+    passing the appropriate time based on the reps counter.
+
+    Args:
+        None
+
+    Returns:
+        None
+    '''
+    global reps
+    reps += 1
+
+    if (reps > 8):
+        reset_timer()
+    elif reps % 8 == 0:
+        header.config(text="Break", fg="red")
+        countdown(LONG_BREAK_SECONDS)
+    elif reps % 2 == 0:
+        header.config(text="Break", fg="yellow")
+        countdown(SHORT_BREAK_SECONDS)
+    else:
+        header.config(text="Work", fg="green")
+        countdown(WORK_SECONDS)
 
 canvas = tk.Canvas(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bg=BACKGROUND_COLOR, highlightthickness=0)
 # chester = tk.PhotoImage(file="chester.png")
@@ -69,10 +92,10 @@ header.grid(column=1, row=1)
 start_button = tk.Button(text="Start", highlightthickness=0, font=(FONT_NAME, 20, "bold"), command=start_timer)
 start_button.grid(column=0, row=3)
 
-reset_button = tk.Button(text="Reset", highlightthickness=0, font=(FONT_NAME, 20, "bold"))
+reset_button = tk.Button(text="Reset", highlightthickness=0, font=(FONT_NAME, 20, "bold"), command=reset_timer)
 reset_button.grid(column=2, row=3)
 
-checkmarks = tk.Label(text="✓", fg="white", bg=BACKGROUND_COLOR, font=(FONT_NAME, 20, "bold"))
+checkmarks = tk.Label(fg="white", bg=BACKGROUND_COLOR, font=(FONT_NAME, 20, "bold"))
 checkmarks.grid(column=1, row=4)
 
 window.mainloop()
